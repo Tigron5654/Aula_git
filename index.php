16756e076e1ff7b2cba64f8b90e2aa9c67381386
Eric Fabian <erickfabiangabp@gmail.com>
2022-07-22 13:14:00 -0400

    V1

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Tests</title>
-</head>
-<body>
+<?php 
 
+echo "Hola mundo!";
 
-
-</body>
-</html>
+?>
